9835d45185dd4b57c5ebb9aa2c550ee73b5e9a4c
Sanne Jamila Olsen <sannejamila@Sannes-MBP.home>
2022-04-02 21:37:09 +0200

    oving9

--- oving9/meeting.cpp
+++ oving9/meeting.cpp
@@ -1,7 +1,6 @@
 #include "meeting.h"
 
 
-
 std::ostream& operator<< ( std::ostream& os, const Campus &campus){
     switch(campus) {
     case Campus::Trondheim:
@@ -16,3 +15,55 @@ std::ostream& operator<< ( std::ostream& os, const Campus &campus){
     }
     return os;
 }
+
+int Meeting::getDay(){
+    return day;
+}
+
+int Meeting::getstartTime(){
+    return startTime;
+}
+
+int Meeting::getendTime(){
+    return endTime;
+}
+
+Campus Meeting::getLocation(){
+    return location;
+}
+
+string Meeting::getSubject(){
+    return subject;
+}
+
+const shared_ptr<Person> Meeting::getLeader(){
+    return leader;
+}
+
+void Meeting::addParticipant(shared_ptr<Person> Person){
+    participants.emplace_back(Person);
+}
+
+ Meeting::Meeting(int day, int startTime, int endTime, Campus location, string subject, const shared_ptr<Person> leader) :
+    day{day}, startTime{startTime}, endTime{endTime}, location{location}, subject{subject}, leader{leader} {
+        Meeting::addParticipant(leader); 
+        }
+ 
+
+ //e) Ved å kalle destruktøren.
+
+ vector<string> Meeting::getParticipantList(){
+     vector <string> participants_name;
+     for(shared_ptr<Person> p : participants){
+         string name = p->getname();
+         participants_name.push_back(name);
+     }
+     return participants_name;
+ }
+
+ ostream& operator<<(ostream& os, Meeting& m){
+    os << "Subject: " << m.getSubject() << " Location: " << m.getLocation() << 
+    " Start: " << m.getstartTime() << "end: " << m.getendTime() << " Name: " << m.getLeader() -> getname() << endl;
+    return os;
+ }
+
--- oving9/meeting.h
+++ oving9/meeting.h
@@ -1,15 +1,35 @@
 #pragma once
 #include <iostream>
+#include "Person.h"
+#include <vector>
 
 using namespace std;
 
 enum class Campus {Trondheim, Ålesund, Gjøvik};
 
-ostream& operator<< ( std::ostream& os, const Campus &campus);
+ostream& operator<< (std::ostream& os, const Campus &campus);
 
 class Meeting{
-    
+    private:
+        int day;
+        int startTime;
+        int endTime;
+        Campus location;
+        string subject;
+        const shared_ptr<Person> leader;
+        vector<shared_ptr<Person>> participants;
+    public:
+        Meeting(int day, int startTime, int endTime, Campus location, string subject, const shared_ptr<Person> leader);
+        int getDay();
+        int getstartTime();
+        int getendTime();
+        Campus getLocation();
+        string getSubject();
+        const shared_ptr<Person> getLeader();
+        void addParticipant(shared_ptr<Person> Person);
+        vector<string> getParticipantList();
 };
 
 
 
+ostream& operator<<(ostream& os, Meeting& m);
