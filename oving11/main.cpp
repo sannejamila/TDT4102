ab5d3da91b1b54be6cca1ad8737d486bf2972d8a
Sanne Jamila Olsen <sannejamila@Sannes-MBP.home>
2022-04-02 22:07:25 +0200

    oving11

--- oving11/main.cpp
+++ oving11/main.cpp
@@ -1,11 +1,29 @@
 #include <vector>
-
+#include <iostream>
+#include "task1.h"
 using namespace std;
 
 int main()
 {
 	vector<string> vec;
 
-	
+	vector<string>::iterator it;
+	vector<string>::reverse_iterator i;
+
+	vec.emplace_back("abc");
+	vec.emplace_back("def");
+	vec.emplace_back("ghi");
+
+	//Iterating
+	for(it = vec.begin(); it < vec.end(); it++){
+		cout << *it << endl;
+	}
+
+	//Reverse iterating
+	for (i = vec.rbegin(); i != vec.rend(); ++i ) { 
+			cout << *i << endl;
+	} 
+
+
 }
 
